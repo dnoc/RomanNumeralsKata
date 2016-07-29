79871dc2e3e54bc866fa3cbaacaa1902f5eedfd9
dnoc <drew.noct@gmail.com>
2016-07-29 14:27:43 -0400

    In-between odd cases, red

--- spec/AuditionSpec.js
+++ spec/AuditionSpec.js
@@ -25,11 +25,17 @@ describe("Audition JavaScript Tests", function() {
   it("converts 50 to Roman Numeral", function() {
         expect(toRoman(50)).toBe("L");});
 
+  it("converts 84 to Roman Numeral", function() {
+        expect(toRoman(50)).toBe("LXXXIV");});
+
   it("converts 90 to Roman Numeral", function() {
         expect(toRoman(90)).toBe("XC");});
 
   it("converts 100 to Roman Numeral", function() {
         expect(toRoman(100)).toBe("C");});
+
+  it("converts 392 to Roman Numeral", function() {
+        expect(toRoman(900)).toBe("CCCXCII");});
 
   it("converts 400 to Roman Numeral", function() {
         expect(toRoman(400)).toBe("CD");});
@@ -40,6 +46,12 @@ describe("Audition JavaScript Tests", function() {
   it("converts 900 to Roman Numeral", function() {
         expect(toRoman(900)).toBe("CM");});
 
+  it("converts 947 to Roman Numeral", function() {
+        expect(toRoman(900)).toBe("CMXLVII");});
+
   it("converts 1000 to Roman Numeral", function() {
+        expect(toRoman(1000)).toBe("M");});
+
+  it("converts 3999 to Roman Numeral", function() {
         expect(toRoman(1000)).toBe("M");});
 });
