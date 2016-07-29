893d63ba2e4799159578cb5375f2e0c9a03788e4
dnoc <drew.noct@gmail.com>
2016-07-29 11:08:55 -0400

    9's and 4's, red

--- spec/AuditionSpec.js
+++ spec/AuditionSpec.js
@@ -22,14 +22,26 @@ describe("Audition JavaScript Tests", function() {
   it("converts 10 to Roman Numeral", function() {
         expect(toRoman(10)).toBe("X");});
 
+  it("converts 40 to Roman Numeral", function() {
+        expect(toRoman(40)).toBe("XL");});
+
   it("converts 50 to Roman Numeral", function() {
         expect(toRoman(50)).toBe("L");});
+
+  it("converts 90 to Roman Numeral", function() {
+        expect(toRoman(90)).toBe("XC");});
 
   it("converts 100 to Roman Numeral", function() {
         expect(toRoman(100)).toBe("C");});
 
+  it("converts 400 to Roman Numeral", function() {
+        expect(toRoman(400)).toBe("CD");});
+
   it("converts 500 to Roman Numeral", function() {
         expect(toRoman(500)).toBe("D");});
+
+  it("converts 900 to Roman Numeral", function() {
+        expect(toRoman(900)).toBe("CM");});
 
   it("converts 1000 to Roman Numeral", function() {
         expect(toRoman(1000)).toBe("M");});
