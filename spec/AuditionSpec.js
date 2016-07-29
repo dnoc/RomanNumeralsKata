2f7a6a806bf9c9bb5e3622bd2ca203e4eb1fcce7
dnoc <drew.noct@gmail.com>
2016-07-29 10:55:34 -0400

    Rest of 1-digit tests, red

--- spec/AuditionSpec.js
+++ spec/AuditionSpec.js
@@ -7,8 +7,17 @@ describe("Audition JavaScript Tests", function() {
   it("converts 1 to Roman Numeral", function() {
         expect(toRoman(1)).toBe("I");});
 
+  it("converts 2 to Roman Numeral", function() {
+          expect(toRoman(2)).toBe("II");});
+
+  it("converts 4 to Roman Numeral", function() {
+        expect(toRoman(4)).toBe("IV");});
+
   it("converts 5 to Roman Numeral", function() {
         expect(toRoman(5)).toBe("V");});
+
+  it("converts 9 to Roman Numeral", function() {
+          expect(toRoman(9)).toBe("IX");});
 
   it("converts 10 to Roman Numeral", function() {
           expect(toRoman(10)).toBe("X");});
