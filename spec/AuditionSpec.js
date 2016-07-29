2ac774f43a03a115892ad740012837bea461d2f4
dnoc <drew.noct@gmail.com>
2016-07-29 10:43:15 -0400

    X test, red

--- spec/AuditionSpec.js
+++ spec/AuditionSpec.js
@@ -10,4 +10,6 @@ describe("Audition JavaScript Tests", function() {
   it("converts 5 to Roman Numeral", function() {
         expect(toRoman(5)).toBe("V");});
 
+  it("converts 10 to Roman Numeral", function() {
+          expect(toRoman(10)).toBe("X");});
 });
