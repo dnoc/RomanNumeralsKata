89be0b629ede7cbc3d78e881b824fd17c5eb1a0e
dnoc <drew.noct@gmail.com>
2016-07-29 10:38:25 -0400

    V test, red

--- spec/AuditionSpec.js
+++ spec/AuditionSpec.js
@@ -7,4 +7,7 @@ describe("Audition JavaScript Tests", function() {
   it("converts 1 to Roman Numeral", function() {
         expect(toRoman(1)).toBe("I");});
 
+  it("converts 5 to Roman Numeral", function() {
+        expect(toRoman(5)).toBe("V");});
+
 });
